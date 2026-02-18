2ca44baf480011739e5b8aa53aacb20f106eca07
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-02-18 17:38:02 -0500

    add script to check version and convert when new version is available

--- inst/scripts/api-docs.R
+++ inst/scripts/api-docs.R
@@ -1,20 +1,40 @@
 # setwd("~/bioc/BiocAzul")
-file_loc <- "inst/service/Azul/api.json"
+file_loc <- "inst/service/azul/api.json"
 
 download.file(
     url = "https://service.azul.data.humancellatlas.org/openapi.json",
     destfile = file_loc
 )
 
-md5 <- digest::digest(file_loc, file = TRUE)
 apilines <- readLines("R/Azul.R")
-mdline <- grep("\"[0-9a-f]{32}\"", apilines, value = TRUE)
-oldmd5 <- unlist(strsplit(mdline, "\""))[[2]]
-updatedlines <- gsub("\"[0-9a-f]{32}\"", dQuote(md5, FALSE), apilines)
+
+.AZUL_LINE <- ".AZUL_API_REFERENCE_VERSION <-"
+
+versionline <- grep(
+    pattern = .AZUL_LINE,
+    x = apilines,
+    fixed = TRUE,
+    value = TRUE
+)
+oldver <- unlist(strsplit(versionline, "\""))[[2L]]
+newver <-
+    jsonlite::fromJSON(file_loc, simplifyVector = FALSE)[[c("info", "version")]]
 
 ## success -- updated API files and MD5
-if (!identical(oldmd5, md5)) {
-    writeLines(updatedlines, con = file("R/Azul.R"))
+if (!identical(oldver, newver)) {
+    lineIdx <- grep(pattern = .AZUL_LINE, x = apilines, fixed = TRUE)
+    apilines[lineIdx] <- paste0(.AZUL_LINE, " \"", newver, "\"")
+    writeLines(apilines, con = file("R/Azul.R"))
+
+    ## update the API file
+    oldwd <- setwd("inst/service/azul")
+    on.exit(setwd(oldwd))
+    system2(
+        command = "api-spec-converter",
+        args = "-f openapi_3 -t swagger_2 openapi.json > api.json",
+        stdout = TRUE
+    )
+
     quit(status = 0)
 } else {
     ## failure -- API the same
